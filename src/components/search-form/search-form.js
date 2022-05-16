584a3be5a665e0e34dfae7c1cfdbd8a05b27f1f7
Vladimir Shits <rav9aa@gmail.com>
2022-05-16 22:21:08 +0300

    added validation for search input

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,18 +1,34 @@
 import style from './search-form.module.css'
 import {setCountriesTC} from "../../bll/reducers/search-reducer";
 import {useDispatch} from "react-redux";
+import {useState} from "react";
 
 export const SearchForm = () => {
     const dispatch = useDispatch()
+    const [searchInputValue, setSearchInputValue] = useState("")
+    const [validationError, setValidationError] = useState(false)
+
+    const onChangeSearchInputValue = (e) => {
+        const RegExpression = /^[a-zA-Z\s]*$/;
+        if( RegExpression.test(e.currentTarget.value)){
+            setSearchInputValue(e.currentTarget.value)
+            validationError && setValidationError(!validationError)
+        } else{
+            setValidationError(true)
+        }
+    }
     const submit = (e) => {
         e.preventDefault()
-        dispatch(setCountriesTC(e.currentTarget[0].value))
-        e.currentTarget.reset()
+        dispatch(setCountriesTC(searchInputValue))
+        setSearchInputValue("")
     }
     return (
         <div className={style.searchForm}>
             <form onSubmit={submit}>
-                <input type={'text'} placeholder={'type country name'}/>
+                <input type={'text'} placeholder={'type country name'}
+                       value={searchInputValue} onChange={onChangeSearchInputValue}
+                />
+                {validationError&& <span>Please use only letters and spaces </span>}
                 <button type={"submit"}>search</button>
             </form>
         </div>
